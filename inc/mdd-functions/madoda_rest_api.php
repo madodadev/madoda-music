35b6c1bbacf1958a5ff363749abc556f50311369
Assedio Horacio <madodasmusic@gmail.com>
2020-04-19 08:23:18 -0700

    comment mdd rest_api

--- inc/mdd-functions/madoda_rest_api.php
+++ inc/mdd-functions/madoda_rest_api.php
@@ -23,69 +23,58 @@ function mdd_rest_get_post($data) {
     // }else {
     //     return "error: no id";
     // }
+}
 
-    $args = array(
-        'numberposts' => -1,
-        'post_type'   => 'post',
-        'post_status' => 'publish'
-    );
-       
-      $all_posts = get_posts( $args );
-      $logs = "";
-      foreach ($all_posts as $key => $post) {
-          $logs = $logs.$post->post_title." ".$post->ID."\n";
-        // $logs = $post->ID;
+function get_madoda_drive_link_format($links) {
+    $mdd_format_ids = "";
+    for($i=0; $i < count($links); $i++){
+        if($i == (count($links) - 1)){
+            $mdd_format_ids = $mdd_format_ids.$links[$i];
+        }else {
+            $mdd_format_ids = $mdd_format_ids.$links[$i]."&&\n";
+        }
     }
-    update_field("google_drive_link",$logs, 313);
-    return $logs;
+
+    return $mdd_format_ids;
 }
 
-// function get_madoda_drive_link_format($links) {
-//     $mdd_format_ids = "";
-//     for($i=0; $i < count($links); $i++){
-//         if($i == (count($links) - 1)){
-//             $mdd_format_ids = $mdd_format_ids.$links[$i];
-//         }else {
-//             $mdd_format_ids = $mdd_format_ids.$links[$i]."&&\n";
-//         }
-//     }
+function update_drive_links($posts_data, $rm_old=true){
+    $cont = 0;
+    foreach ($posts_data as $key => $post_data) {
+       $wp_id = $post_data["id"];
+       $drive_links = get_madoda_drive_link_format($post_data["drive_links"]);
+       if($rm_old) {
+           if(get_post( $wp_id)) {
+               update_field("google_drive_link",$drive_links, $wp_id);
+               $cont = "wp_id";
+           }
+       }else {
+           //append to old ids
+       }
+    }
 
-//     return $mdd_format_ids;
-// }
-// function mdd_ceack_links_type($links) {
-//     if(is_array( $links["drive_links"] ) ) {
-//         return "multi musics";
-//     }else {
-//         return "one musics";
-//     }
-// }
-// function mdd_rest_edit_post($data) {
-//     if($data["id"]){
-        
-//         $id = $data["id"];
-//         $drive_links = get_madoda_drive_link_format($data["drive_links"]);
-        
-//         if($data["rm_old_dive_links"] != "no") {
-//             //substitur drive ids
-//             // update_field("google_drive_link",$drive_links, $id);
-//             return mdd_ceack_links_type($data["drive_links"]);
-//         }else {
-//             //adcionar drive ids
-//             return "not working for now";
-//         } 
-//     }else{
-//         return "error: no id";
-//     }
-// }
+    return $cont;
+}
+
+function mdd_rest_edit_post($data) {
+    if(true){
+        $post_data = $data["data"]['posts'];
+        $settings = $data["data"]["settings"];
+        if($settings["type"] == "gdrive"){
+            return update_drive_links($post_data);
+        }
+    }
+}
+
 function madoda_registe_custom_rest_api() {
-    register_rest_route( "madoda/v1", "post", array(
-        "methods" => WP_REST_SERVER::READABLE,
-        "callback" => "mdd_rest_get_post"
-    ) );
-    
     // register_rest_route( "madoda/v1", "post", array(
-    //     "methods" => WP_REST_SERVER::EDITABLE,
-    //     "callback" => "mdd_rest_edit_post"
+    //     "methods" => WP_REST_SERVER::READABLE,
+    //     "callback" => "mdd_rest_get_post"
     // ) );
+    
+    register_rest_route( "madoda/v1", "edit/posts", array(
+        "methods" => WP_REST_SERVER::EDITABLE,
+        "callback" => "mdd_rest_edit_post"
+    ) );
 }
-add_action("rest_api_init", "madoda_registe_custom_rest_api");
+// add_action("rest_api_init", "madoda_registe_custom_rest_api");
