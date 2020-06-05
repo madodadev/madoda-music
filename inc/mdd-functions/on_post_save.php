5618a831b9fe03eddf43be47806931a3749c5255
Assedio Horacio <madodasmusic@gmail.com>
2020-06-05 13:48:33 -0700

    change date tag to get_the_date

--- inc/mdd-functions/on_post_save.php
+++ inc/mdd-functions/on_post_save.php
@@ -8,19 +8,19 @@ function mdd_update_category_and_tags($post_id) {
     foreach ($artists as $artist) {
         $ar_tags = array(
             $artist->post_title,
-            $artist->post_title . " " . date("Y"),
-            $artist->post_title . " ". date("m/Y")
+            $artist->post_title . " " . get_the_date("Y", $post_id),
+            $artist->post_title . " " . get_the_date("m/Y", $post_id)
         );
         foreach($ar_tags as $tag) {
             wp_set_post_tags( $post_id, $tag, true );
         }
     }
     $date_tags = array(
-        date("Y"),
-        date("m/Y"),
-        date("d/m/Y")
+        get_the_date("Y", $post_id),
+        get_the_date("m/Y", $post_id),
+        get_the_date("d/m/Y", $post_id)
     );
-
+    
     wp_set_post_tags( $post_id, $date_tags, true );
 
     $seo_tags = array(
