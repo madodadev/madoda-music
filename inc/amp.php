c10314ae79dee952dd44d9f86c1e5ec2c65260c5
Assedio Horacio <madodasmusic@gmail.com>
2020-07-20 08:18:42 -0700

    try diferent amp schema 01

--- inc/amp.php
+++ inc/amp.php
@@ -5,18 +5,15 @@ function mddm_the_amp_head() {
         <script type="application/ld+json">
             {
                 "@context": "http://schema.org",
-                "@type": "MusicGroup",
-                "headline": "<?php echo get_the_title()?>",
+                "@type": "MusicObject",
                 "datePublished":"<?php echo get_the_date()?>",
                 "name": "<?php echo mdd_get_artist()?> - <?php mdd_the_title()?>",
                 "genre":"<?php echo get_the_category()[0]->name;?>",
                 "byArtist":"<?php echo mdd_get_artist()?>",
-                "url":"<?php the_permalink(); ?>",
+                "contentURL":"<?php the_permalink(); ?>",
                 "image": [
                 "<?php mdd_the_single_front_imgUrl()?>"
-                ],
-                "keywords":"Baixar musica de <?php echo mdd_get_artist()?> <?php mdd_the_title()?>, download musics, mp3 download"
-            }
+                ]            }
         </script>
         <?php
     }else {?>
