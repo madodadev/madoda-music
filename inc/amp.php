1dcdf0d372d58e599dd25bdfa222168667a9de5b
Assedio Horacio <madodasmusic@gmail.com>
2020-07-20 14:37:27 -0700

    try diferent amp schema 02

--- inc/amp.php
+++ inc/amp.php
@@ -5,15 +5,12 @@ function mddm_the_amp_head() {
         <script type="application/ld+json">
             {
                 "@context": "http://schema.org",
-                "@type": "MusicObject",
-                "datePublished":"<?php echo get_the_date()?>",
-                "name": "<?php echo mdd_get_artist()?> - <?php mdd_the_title()?>",
-                "genre":"<?php echo get_the_category()[0]->name;?>",
-                "byArtist":"<?php echo mdd_get_artist()?>",
-                "contentURL":"<?php the_permalink(); ?>",
+                "@type": "MusicGroup",
+                "headline": "<?php echo get_the_title()?>",
                 "image": [
                 "<?php echo get_the_post_thumbnail_url()?>"
-                ]            }
+                ]            
+            }
         </script>
         <?php
     }else {?>
