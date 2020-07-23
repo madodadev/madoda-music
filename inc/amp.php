0f6df59e0ec546dc3a11ce60e570ee7940a718c8
Assedio Horacio <madodasmusic@gmail.com>
2020-07-22 17:46:42 -0700

    modify head post data structure

--- inc/amp.php
+++ inc/amp.php
@@ -1,25 +1,118 @@
 <?php
 
 function mddm_head_post_structure() {
+    function get_images_url() {
+        if(get_the_post_thumbnail_url()) {
+            $images = [
+                get_the_post_thumbnail_url()
+            ];
+
+            if( get_the_post_thumbnail_url(get_the_ID(), "desplay") ) array_push($images, get_the_post_thumbnail_url(get_the_ID(), "desplay"));
+            if( get_the_post_thumbnail_url(get_the_ID(), "playlistDesplay") ) array_push($images, get_the_post_thumbnail_url(get_the_ID(), "playlistDesplay"));
+            if( get_the_post_thumbnail_url(get_the_ID(), "singleDesplay") ) array_push($images, get_the_post_thumbnail_url(get_the_ID(), "singleDesplay"));
+            if( get_the_post_thumbnail_url(get_the_ID(), "singleBackground") ) array_push($images, get_the_post_thumbnail_url(get_the_ID(), "singleBackground"));
+            return $images;
+        }
+
+        return false;
+    }
+
+
     if(  is_single() AND in_array(get_post_type(), ['post','album','playlist'])  ){?>
-        <script type="application/ld+json">
-            {
-                "@context": "http://schema.org",
-                "@type": "MusicGroup",
-                "headline": "<?php echo get_the_title()?>",
-                "image": [
-                "<?php mdd_the_single_back_imgUrl();?>"
-                ]            
+     <?php   
+        $schema = array(
+            "@context" => "http://schema.org",
+            "@type" => "MusicGroup",
+            "track" => array(
+                "@type" => "MusicRecording",
+                "audio" => get_the_permalink(),
+                "url" => get_the_permalink()
+            )
+        );
+
+        if ( get_comments_number() ) {
+            $schema["track"]["interactionStatistic"] = array(
+                "@type" => "InteractionCounter",
+                "interactionType" => get_the_permalink(),
+                "userInteractionCount" => get_comments_number()
+            );
+        }
+
+        if( get_the_category()[0] ) {
+            $gener = get_the_category()[0]->name;
+            $schema["gener"] = $gener;
+        }
+
+        if( mdd_get_artist() AND mdd_get_title() ) {
+            $schema["track"]["name"] = mdd_get_artist()." - ".mdd_get_title();
+            $schema["name"] = mdd_get_artist()." - ".mdd_get_title();
+        
+        }else {
+            $schema["name"] = get_the_title();
+        }
+
+        if( get_images_url() ) {
+            $schema["image"] = get_images_url();
+        }
+
+        if( get_field('youtube_video_url') ) {
+            $video_schema = array(
+                "@type" => "VideoObject",
+                "url" => get_the_permalink(),
+                "contentUrl" => get_field('youtube_video_url'),
+                "embedUrl" => get_the_permalink()
+            );
+
+            if( mdd_get_artist() AND mdd_get_title() ){
+                $video_schema["name"] = mdd_get_artist()." - ".mdd_get_title();
+                $video_schema["description"] = "video de " .mdd_get_artist()." com o titlo ".mdd_get_title();
             }
-        </script>
-        <?php
-    }else {?>
+
+            if( get_images_url() ) {
+                $video_schema["thumbnailUrl"] = get_images_url();
+            }
+
+            $schema["subjectOf"] = $video_schema;
+        }
+            
+    ?>    
         <script type="application/ld+json">
-            {
-                "@context": "http://schema.org",
-                "@type": "MusicGroup",
-                "headline": "<?php echo get_the_title()?>"
-           }
+            <?php echo json_encode($schema); ?>
+        </script>
+    <?php
+    }else {
+        
+        $schema = array(
+            "@context" => "http://schema.org",
+            "@type" => "NewsArticle",
+            "headline" => get_the_title(),
+            "mainEntityOfPage" => array(
+                "@type" => "WebPage",
+                "@id" => get_the_permalink()
+            ),
+            "datePublished" => get_the_date("y-m-dTH:m:s"),
+            "dateModified" => get_the_modified_date( "y-m-dTH:m:s" ),
+            "author" => array(
+                "@type" => "Person",
+                "name" => get_the_author()
+            ),
+            "publisher" => array(
+                "@type" => "Organization",
+                "name" => get_the_author(),
+                "logo" => array(
+                    "@type" => "ImageObject",
+                    "url" =>  get_theme_file_uri("/assets/images/mddm512.png")
+                )
+            )
+        );
+        
+        if( get_images_url() ) {
+            $schema["image"] = get_images_url();
+        }
+        
+    ?>
+        <script type="application/ld+json">
+            <?php echo json_encode($schema); ?>
         </script>
     <?php
     }   
