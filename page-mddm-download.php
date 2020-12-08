d9ed579837ce143d9fe023770f6b3c154873b9e7
Assedio Horacio <madodadev@gmail.com>
2020-12-08 17:51:05 +0200

    change download text in mddm-download

--- page-mddm-download.php
+++ page-mddm-download.php
@@ -1,6 +1,7 @@
 <?php get_header();?>
 <?php
 $download_post_id = sanitize_text_field(get_query_var('of'));
+
 $download_music = new WP_Query(array(
     "post__in" => array($download_post_id)
 ))
@@ -19,7 +20,7 @@ $download_music = new WP_Query(array(
 <?php 
     $newmusics = new WP_Query(array(
         'post_type' => 'post',
-        'posts_per_page' => 24
+        'posts_per_page' => 10
     ));
 ?>
 
@@ -33,11 +34,11 @@ $download_music = new WP_Query(array(
     ));
 ?>
 
+<section class="main" id="download-single" style="color:var(--main-fk-color)">
+<div class="container">
 <?php
     while($download_music->have_posts()):
         $download_music->the_post();?>
-        <section class="main" id="download-single" style="color:var(--main-fk-color)">
-        <div class="container">
             <div class="top-info" >
             <h2 class="title"><?php echo mdd_get_artist()." ". mdd_get_title()?></h2>
             <?php dynamic_sidebar( 'single-top-sidebar' ); ?>
@@ -45,94 +46,95 @@ $download_music = new WP_Query(array(
             <?php if(mdd_get_download_num() > 50): ?>
                 <p class="download-count"><span id="download-num"><?php echo mdd_get_download_num()." "?></span> Downloads <img class="download-icon" src="<?php echo get_theme_file_uri('/assets/icons/download.svg')?>" alt="download icon"></p>
             <?php endif;?>
-            <p style='font-size:20px'>Se o downlaod não iniciar automaticamente dentro de alguns segundos</p>
+            <p style='font-size:20px'>Se o downlaod não iniciar automaticamente dentro de alguns segundos clica em</p>
             <a id="download-btn" class="download-btn" data_music="<?php the_ID();?>" data_artist="<?php mdd_the_artist_id()?>"  href="<?php echo mdd_get_download_audio_url()?>" traget="_blank">
                 <p class="download-btn-text">
-                    <strong style="color:var(--destak-color)">click Aqui para baixar </strong> 
+                    <strong style="color:var(--destak-color)">Downlaod MP3 </strong> 
                 </p>
             </a>
             </dev>
-            <?php dynamic_sidebar( 'single-top-sidebar' ); ?>
-            <div class="topmusic posts-cont">
-                <div class="header">
-                <?php madoda_scroll_card_header(array(
-                    'name'  =>  'Musicas em Alta',
-                    'targetScroll'  => 'topmusica-wrap',
-                    'see_more'  =>  array(
-                        'link'  =>  site_url("/trending")
-                    )
-                ))?>
-                </div>
-                
-                <div id="topmusica-wrap" class="wrapper-card">
-                    <?php
-                        while($topmusics->have_posts()){
-                            $topmusics->the_post();
-                            get_template_part('template-parts/music');
-                        }
-                        wp_reset_postdata();
-                    ?>
-                </div>
-            
-            </div>
-
-            <?php dynamic_sidebar( 'music-info-ads-sidebar' ); ?>
-            
-            <div class="Novas posts-cont">
-                <span class="cate_name"><a href="<?php echo site_url("/musics")?>">Novas Musicas</a></span>
-                <div class="wrapper-card-list">    
-                    <?php 
-                        while($newmusics->have_posts()){
-                            $newmusics->the_post();
-                            get_template_part('template-parts/music', 'list');
-                        }
-                        wp_reset_postdata();
-                    ?>
-                </div>
-                <div class="see-more-list"><a href="<?php echo site_url("/musics")?>">ver Todas</a></div>
-            </div>
-
-            <?php dynamic_sidebar( 'music-info-ads-sidebar' ); ?>
-
-            <div class="topauthor posts-cont">
-                <div class="header">
-                    <?php 
-                    madoda_scroll_card_header(array(
-                        'name'  =>  'Artistas',
-                        'targetScroll'  => 'cantores-wrap',
-                        'see_more'  =>  array(
-                            'link'  =>  mdd_get_archiveUrl("artist")
-                            
-                        )
-                    ))?>
-                </div>
-
-                <div id="cantores-wrap" class="wrapper-card">       
-                    <?php 
-                        while($singer->have_posts()){
-                            $singer->the_post();
-                            get_template_part('template-parts/artist');
-                        }
-                        wp_reset_postdata();
-                    ?>
-                </div>
-            </div>
-         </dev>
-         </section>
-         <script>
-            let download_btn = document.querySelector("#download-btn")
-            let music_id =  download_btn.attributes.data_music.value;
-            let mdd_download = sessionStorage.getItem(music_id);
-            setTimeout(() => {
-                if( mdd_download != "downloaded") {
-                    download_btn.click();
-                }
-            }, 5000);
-            
-            
-        </script>
-    <?php
+            <?php
     endwhile;
     wp_reset_postdata();
-?>
+    ?>
+    <?php dynamic_sidebar( 'single-top-sidebar' ); ?>
+    <div class="topmusic posts-cont">
+        <div class="header">
+        <?php madoda_scroll_card_header(array(
+            'name'  =>  'Musicas Novas',
+            'targetScroll'  => 'newmusica-wrap',
+            'see_more'  =>  array(
+                'link'  =>  site_url("/musics")
+            )
+        ))?>
+        </div>
+        
+        <div id="newmusica-wrap" class="wrapper-card">
+            <?php
+                while($newmusics->have_posts()){
+                    $newmusics->the_post();
+                    get_template_part('template-parts/music');
+                }
+                wp_reset_postdata();
+            ?>
+        </div>
+    
+    </div>
+
+    <?php dynamic_sidebar( 'music-info-ads-sidebar' ); ?>
+    
+    <div class="treding posts-cont">
+        <span class="cate_name"><a href="<?php echo site_url("/musics")?>">Musicas Em Alta</a></span>
+        <div class="wrapper-card-list">    
+            <?php 
+                while($topmusics->have_posts()){
+                    $topmusics->the_post();
+                    get_template_part('template-parts/music', 'list');
+                }
+                wp_reset_postdata();
+            ?>
+        </div>
+        <div class="see-more-list"><a href="<?php echo site_url("/musics")?>">ver Todas</a></div>
+    </div>
+
+    <?php dynamic_sidebar( 'music-info-ads-sidebar' ); ?>
+
+    <div class="topauthor posts-cont">
+        <div class="header">
+            <?php 
+            madoda_scroll_card_header(array(
+                'name'  =>  'Artistas',
+                'targetScroll'  => 'cantores-wrap',
+                'see_more'  =>  array(
+                    'link'  =>  mdd_get_archiveUrl("artist")
+                    
+                )
+            ))?>
+        </div>
+
+        <div id="cantores-wrap" class="wrapper-card">       
+            <?php 
+                while($singer->have_posts()){
+                    $singer->the_post();
+                    get_template_part('template-parts/artist');
+                }
+                wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+</dev>
+</section>
+    <script>
+    let download_btn = document.querySelector("#download-btn")
+    let music_id =  download_btn.attributes.data_music.value;
+    let mdd_download = sessionStorage.getItem(music_id);
+    setTimeout(() => {
+        if( mdd_download != "downloaded") {
+            download_btn.click();
+        }
+    }, 5000);
+        
+        
+    </script>
+
 <?php get_footer();?>
